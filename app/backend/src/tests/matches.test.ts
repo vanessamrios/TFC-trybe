167a51bbc0d5425649b3e4d191c190992bbd91f7
Vanessa <vanessamrios@gmail.com>
2022-06-09 12:43:21 +0200

    (Req 22) Cria testes para o endpoint post /matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -7,12 +7,13 @@ import { app } from '../app';
 
 import { Response } from 'superagent';
 import Matches from '../database/models/Matches';
+import * as jwtGenerator from '../helpers/jwtGenerator';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testa o endpoint /teams', () => {
+describe('Testa o endpoint /matches', () => {
 
   let chaiHttpResponse: Response;
 
@@ -20,10 +21,25 @@ describe('Testa o endpoint /teams', () => {
     sinon
       .stub(Matches, "findAll")
       .resolves();
+    sinon
+      .stub(Matches, "create")
+      .resolves({
+        id: 1,
+        homeTeam: 16,
+        homeTeamGoals: 2,
+        awayTeam: 8,
+        awayTeamGoals: 2,
+        inProgress: true,
+      } as Matches);
+    sinon
+      .stub(jwtGenerator, "verify")
+      .resolves();
   });
 
   afterEach(()=>{
     (Matches.findAll as sinon.SinonStub).restore();
+    (Matches.create as sinon.SinonStub).restore();
+    (jwtGenerator.verify as sinon.SinonStub).restore();
   })
 
   it('Retorna um status 200 e uma lista de partidas', async () => {
@@ -32,6 +48,46 @@ describe('Testa o endpoint /teams', () => {
        .get('/matches')
 
     expect(chaiHttpResponse.status).to.be.equal(200)
+  });
+
+  it('Retorna um status 201 e os dados da partida criada', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .set('authorization', '123')
+      .send( {
+        homeTeam: 16,
+        awayTeam: 8,
+        homeTeamGoals: 2,
+        awayTeamGoals: 2,
+        inProgress: true
+      })
+      const responseBody = {
+        id: 1,
+        homeTeam: 16,
+        homeTeamGoals: 2,
+        awayTeam: 8,
+        awayTeamGoals: 2,
+        inProgress: true,}
+    expect(chaiHttpResponse.status).to.be.equal(201);
+    expect(chaiHttpResponse.body).to.be.eql(responseBody)
+
+  });
+
+  it('Retorna um status 401 se não houver token', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .send( {
+        homeTeam: 16,
+        awayTeam: 8,
+        homeTeamGoals: 2,
+        awayTeamGoals: 2,
+        inProgress: true
+      })
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.be.eql({ message: 'Token not found' })
+
   });
 
 });
